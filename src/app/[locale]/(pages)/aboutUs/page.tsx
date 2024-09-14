77dcd4b771a9a8ac6a24ec8615e1188abcf4d82e
georgiginev <gosho.ginev@gmail.com>
2024-09-14 19:44:42 +0300

    Added metadata to aboutUs page

--- src/app/[locale]/(pages)/aboutUs/page.tsx
+++ src/app/[locale]/(pages)/aboutUs/page.tsx
@@ -1,4 +1,11 @@
+import { Metadata } from "next";
 import Title from "./title";
+
+export const metadata: Metadata = {
+    title: process.env.TITLE_PREFIX as string + ' - AboutUs',
+    description: 'About us information'
+};
+
 
 const Page = () => {
     return (<section>
