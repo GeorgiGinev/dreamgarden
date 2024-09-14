b69fb5731b0e0b2817658deb3ba5170cfaf5728c
georgiginev <gosho.ginev@gmail.com>
2024-09-14 19:42:45 +0300

    Fixed layout and created aboutUs page

--- src/interfaces/page.interface.tsx
+++ src/interfaces/page.interface.tsx
@@ -2,5 +2,8 @@ export interface PageTitleInterface {
     primaryTitle: string | undefined,
     setPrimaryTitle: (data: string) => void,
     secondaryTitle: string | undefined,
-    setSecondaryTitle: (data: string) => void
+    setSecondaryTitle: (data: string) => void,
+    isContactFormVisible: boolean,
+    setIsContactFormVisible: (isVisible: boolean) => void,
+    setPageImage: (image: string) => void
 }
