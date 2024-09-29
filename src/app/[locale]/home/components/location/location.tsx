2575d7df5484c8f7a93d73e153f89f8d0a543aa6
georgiginev <gosho.ginev@gmail.com>
2024-09-29 15:20:57 +0300

    location headings update

--- src/app/[locale]/home/components/location/location.tsx
+++ src/app/[locale]/home/components/location/location.tsx
@@ -14,13 +14,13 @@ const HomeLocation = (params: LocationInterface) => {
             <Col xs={12} lg={4}>
                 <div className={styles['information-container-wrapper'] + " bg-primary px-3 py-4"}>
                     <div className="mb-3">
-                        <h5 className="mb-3">{homeTranslation('locationHomeAddressTitle')}</h5>
+                        <h2 className="mb-3">{homeTranslation('locationHomeAddressTitle')}</h2>
                         <p>
                             {params.contacts.specificAddress}
                         </p>
                     </div>
                     <div className="mb-3">
-                        <h5 className="mb-3">{homeTranslation('locationHomeContactTitle')}</h5>
+                        <h2 className="mb-3">{homeTranslation('locationHomeContactTitle')}</h2>
                         <p>
                             {params.contacts.phoneNumbers[0]}
                             <br />
