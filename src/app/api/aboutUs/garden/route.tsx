784363b5baf61ef9088ce61ae990b15f312113e0
georgiginev <gosho.ginev@gmail.com>
2024-09-30 21:31:17 +0300

    Example for a request to backend

--- src/app/api/aboutUs/garden/route.tsx
+++ src/app/api/aboutUs/garden/route.tsx
@@ -5,6 +5,9 @@ import { createAboutUsGardenMock } from "@/services/aboutUs/garden/mocks/garden.
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
+    // const response = await fetch('https://api.example.com/data');
+    // const data = await response.json();
+
     //const data = await request.json(); // Извличане на JSON данни от POST заявката
     const data: AboutUsGardenApiResponseInterface = createAboutUsGardenMock();
     return NextResponse.json(data, { status: 200 });
